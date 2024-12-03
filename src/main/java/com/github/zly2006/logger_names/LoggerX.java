6ddd38b278b25707897804d891d19741c8bac17e
Liyan Zhao <return65535@qq.com>
2024-12-03 23:17:36 +0800

    fix

--- src/main/java/com/github/zly2006/logger_names/LoggerX.java
+++ src/main/java/com/github/zly2006/logger_names/LoggerX.java
@@ -1,21 +1,25 @@
 package com.github.zly2006.logger_names;
 
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.loader.api.FabricLoader;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.core.LifeCycle;
 import org.apache.logging.log4j.core.LoggerContext;
 import org.apache.logging.log4j.core.config.Configuration;
 import org.apache.logging.log4j.core.config.ConfigurationFactory;
-import org.apache.logging.log4j.core.config.plugins.util.PluginManager;
-import org.apache.logging.log4j.core.config.plugins.util.PluginRegistry;
 import org.slf4j.Logger;
 
 import java.net.URISyntaxException;
 
 public class LoggerX implements ModInitializer {
     public static final Logger LOGGER = org.slf4j.LoggerFactory.getLogger("LoggerX");
+
     @Override
     public void onInitialize() {
+        if (FabricLoader.getInstance().isDevelopmentEnvironment()) {
+            LOGGER.info("LoggerX: Development environment detected, skipping reconfiguration.");
+            return;
+        }
         if (LogManager.getContext(LoggerX.class.getClassLoader(), false) instanceof LoggerContext context) {
             if (context.getState() != LifeCycle.State.STARTED) {
                 LOGGER.error("LoggerX: LoggerContext is not started, cannot reconfigure.");
